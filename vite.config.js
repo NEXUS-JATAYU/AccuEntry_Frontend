b1a4f79a991e6e61c9782f4addee02bdfe9d32a9
artistmait <maitreyeepuranik@gmail.com>
2026-03-06 17:58:13 +0530

    fix: vercel fix on vite config

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   // eslint-disable-next-line no-undef
-  base : process.env.VITE_BASE_PATH || '/AccuEntry_Frontend',
+  base : process.env.VITE_BASE_PATH || '/',
 })
